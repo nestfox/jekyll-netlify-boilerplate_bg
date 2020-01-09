d84c2c7f5d7435531ee8a992931cd105397af20f
madrasbox <59695464+madrasbox@users.noreply.github.com>
2020-01-10 01:37:58 +0530

    Update custom_2020JAN.js

--- assets/custom_2020JAN.js
+++ assets/custom_2020JAN.js
@@ -1,16 +1,16 @@
-  <script>
   var myIndex = 0;
   carousel();
 
   function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-  x[i].style.display = "none";  
+      var i;
+      var x = document.getElementsByClassName("mySlides");
+      for (i = 0; i < x.length; i++) {
+          x[i].style.display = "none";
+      }
+      myIndex++;
+      if (myIndex > x.length) {
+          myIndex = 1
+      }
+      x[myIndex - 1].style.display = "block";
+      setTimeout(carousel, 5000);
   }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 5000);
-  }
-</script>
